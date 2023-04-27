4e9abfc9bd2d5cb526498df29290ad2406d84f33
sabuja <gpattnaik1234@gmail.com>
2023-04-27 16:19:51 +0530

    all files added

--- SpringAngular/src/main/java/com/Project/SpringAngular/Service/CustomerService.java
+++ SpringAngular/src/main/java/com/Project/SpringAngular/Service/CustomerService.java
@@ -1,26 +1,76 @@
 package com.Project.SpringAngular.Service;
+
+import com.Project.SpringAngular.CustomerRepo.CustomerRepo;
 import com.Project.SpringAngular.DTO.CustomerDTO;
 import com.Project.SpringAngular.DTO.CustomerSaveDTO;
 import com.Project.SpringAngular.DTO.CustomerUpdateDTO;
+import com.Project.SpringAngular.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface CustomerService {
-//    static String addCustomer(CustomerSaveDTO customerSaveDTO) {
-//
-//        return null;
-//    }
+@Service
+public class CustomerServiceIMPL implements CustomerService{
+    @Autowired
+    private CustomerRepo customerRepo;
 
-    static String addCustomer(CustomerSaveDTO customerSaveDTO) {
+
+
+    @Override
+    public String addCustomer(CustomerSaveDTO customerSaveDTO) {
+        Customer customer= new Customer(
+                customerSaveDTO.getCustomername(),
+                customerSaveDTO.getCustomeraddress(),
+                customerSaveDTO.getMobile(),
+                customerSaveDTO.getAge()
+        );
+        customerRepo.save(customer);
+        return customer.getCustomername();
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomer() {
+        List<Customer> getCustomers=customerRepo.findAll();
+        List<CustomerDTO> customerDTOList=new ArrayList<>();
+        for(Customer a:getCustomers){
+            CustomerDTO customerDTO=new CustomerDTO(
+                    a.getCustomerid(),
+                    a.getCustomername(),
+                    a.getCustomeraddress(),
+                    a.getMobile(),
+                    a.getAge()
+            );
+            customerDTOList.add(customerDTO);
+        }
+        return customerDTOList;
+    }
+
+    public String updateCustomers(CustomerUpdateDTO customerUpdateDTO){
+        if(customerRepo.existsById(customerUpdateDTO.getCustomerid()))
+        {
+            Customer customer=customerRepo.getById(customerUpdateDTO.getCustomerid());
+            customer.setCustomername(customerUpdateDTO.getCustomername());
+            customer.setCustomeraddress(customerUpdateDTO.getCustomeraddress());
+            customer.setMobile(customerUpdateDTO.getMobile());
+            customer.setAge(customerUpdateDTO.getAge());
+            customerRepo.save(customer);
+        }
+        else{
+            System.out.println("Customer ID Do not Exist");
+        }
         return null;
     }
 
-
-    List<CustomerDTO> getAllCustomer();
-
-    static String updateCustomers(CustomerUpdateDTO customerUpdateDTO) {
-        return null;
+    @Override
+    public boolean deleteCustomer(int id) {
+        if(customerRepo.existsById(id)){
+            customerRepo.deleteById(id);
+        }
+        else{
+            System.out.println("customer id doesn't exist");
+        }
+        return true;
     }
-
-    boolean deleteCustomer(int id);
 }
